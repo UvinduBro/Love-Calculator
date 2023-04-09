3bc86cc55794ee968e9fc942bb4ecf09c70e4620
Uvindu Rajapakshe <118661330+UvinduBro@users.noreply.github.com>
2023-04-09 21:14:46 +0000

    new function:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function calculateLoveCompatibility(name1, name2) {
     const percentage = totalAscii % 101; // Limit the result to be between 0 and 100
     return percentage;
 }
+
+
+/* function getRandomQuote() {
+    // Define an array of quotes
+    const quotes = [
+        "Love is not finding someone to live with; it's finding someone you can't live without.",
+        "The best thing to hold onto in life is each other.",
+        "Love is the greatest refreshment in life.",
+        "The best and most beautiful things in this world cannot be seen or even heard, but must be felt with the heart.",
+        "Love is when the other person's happiness is more important than your own.",
+        "Where there is love, there is life.",
+        "Being deeply loved by someone gives you strength, while loving someone deeply gives you courage.",
+        "Love doesn't make the world go 'round. Love is what makes the ride worthwhile."
+    ];
+
+    // Return a random quote from the array
+    return quotes[Math.floor(Math.random() * quotes.length)];
+}
+*/
